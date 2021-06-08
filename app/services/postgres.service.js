0d0f6698f321ce060c48da7e6f993254577aed59
Mariana Palacios <32974758+mariana458@users.noreply.github.com>
2021-06-07 20:39:05 -0500

    cambio puerto

--- app/services/postgres.service.js
+++ app/services/postgres.service.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 
 class PostgresService {
     constructor(){
-        let uri= process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/prueba-tecnica"
+        let uri=  "postgres://yvywibvsdzuehj:e98369e42475d0cb18298276aad7f43c6591ff702f5399ee407d86978403be65@ec2-3-233-7-12.compute-1.amazonaws.com:5432/dc38r8ud52mci9"
         this.connectionString= uri
         this.pool=new pg.Pool(
             {connectionString:this.connectionString}
